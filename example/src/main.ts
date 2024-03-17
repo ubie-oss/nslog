1856c1270f6aed4d274bfb0c9679bf0dddb358f1
Kazuhito Hokamura <k.hokamura@gmail.com>
2024-03-17 09:24:33 +0900

    Fix example

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger, ValidationPipe } from "@nestjs/common";
+import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { Module } from "@nestjs/common";
 import { StructuredLogger } from "./structured-logger";
@@ -8,11 +8,13 @@ export class AppModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-  app.useLogger(new StructuredLogger({ logLevel: "verbose", format: "text" }));
+  app.useLogger(new StructuredLogger({ logLevel: "verbose", format: "json" }));
 
   const logger = new Logger("bootstrap");
-  logger.log("Application is starting...", { foo: "bar" });
-  logger.error({ error: "something went wrong" }, "extra params");
+  logger.log("Hello structured log!", { foo: "bar" });
+  logger.debug("This is a debug log", { x: { y: "z" } });
+  logger.warn("This is a warning");
+  logger.error("something went wrong", { errorCode: "xxx", result: "error" });
 
   await app.listen(3000);
   logger.log(`Application is running on: ${await app.getUrl()}`);
